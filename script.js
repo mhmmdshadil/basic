071d4f3e3c0db7b9148af1e827c07bf977a7449d
Muhammed Shadil <muhammedshadilmp7@gmail.com>
2026-06-07 17:31:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-alert('setup');
+let userName = prompt("who's there? ",'');
+if (userName == null || userName == ''){
+    alert("cancelled")
+    window.close()
+}
+else if(userName == "Admin"){
+    let pass = prompt("enter the password",'');
+    if (pass == "TheMaster"){
+        alert("you're welcome!")
+    }
+    else if (pass == null || pass == ''){
+    alert("cancelled")
+    window.close()
+    }
+    else{
+    alert("I don't know you!")
+    window.close()
+    }
+}
+
+else{
+    alert("I don't know you!")
+    window.close()
+}
